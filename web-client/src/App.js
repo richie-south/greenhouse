48bec499b71bbac958eaf0c2b4d20aea6047f341
richie-south <richard.soderman@gmail.com>
2018-09-16 18:32:36 +0200

    refactor: removed unused files

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
@@ -14,8 +12,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
